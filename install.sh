8fb9df5a1b303030f308daf0b420b40ac498bbe1
oqo0 <oqqqqqqqqqqq@gmail.com>
2022-05-25 13:15:41 +0300

    Configure ufw

--- install.sh
+++ install.sh
@@ -10,7 +10,23 @@ sudo chpasswd <<<"root: ${NEW_PASSWORD}"
 sudo apt update
 sudo apt install fail2ban
 
-
-
+# configuring ufw
+sudo ufw default allow outgoing
+sudo ufw default deny incoming
+# generating a new random ports
+NEW_SSH_PORT=$(shuf -i 2000-9999 -n 1)
+NEW_HTTP_PORT=$(shuf -i 10000-29999 -n 1)
+NEW_HTTPS_PORT=$(shuf -i 30000-65000 -n 1)
+# allow this ports and limit ssh
+sudo ufw allow NEW_SSH_PORT
+sudo ufw limit NEW_SSH_PORT
+sudo ufw allow NEW_HTTP_PORT
+sudo ufw allow NEW_HTTPS_PORT
+# change ssh port to a new one
+sudo sed -i 's/#Port 22/#Port ${NEW_SSH_PORT}/' /etc/ssh/sshd_config
+sudo systemctl restart ssh
+# apply new ufw rules
+sudo ufw enable
 
 echo "Root password: ${NEW_PASSWORD}"
+echo "SSH port: ${NEW_SSH_PORT}"
