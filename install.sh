87434f50d969401ff31d495e2e748b592131819d
Findlim <43876485+Findlim@users.noreply.github.com>
2023-02-24 15:43:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # generating a new strong random password
-NEW_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32 ; echo '')
+# NEW_PASSWORD=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 32 ; echo '')
 # setting a new password
-sudo echo -e "${NEW_PASSWORD}\n${NEW_PASSWORD}" | passwd root
+# sudo echo -e "${NEW_PASSWORD}\n${NEW_PASSWORD}" | passwd root
 
 # install fail2ban 
 # it will start working automatically
@@ -14,16 +14,17 @@ sudo apt install -y fail2ban
 sudo ufw default allow outgoing
 sudo ufw default deny incoming
 # generating a new random ports
-NEW_SSH_PORT=$(shuf -i 2000-9999 -n 1)
-NEW_HTTP_PORT=$(shuf -i 10000-29999 -n 1)
-NEW_HTTPS_PORT=$(shuf -i 30000-65000 -n 1)
+# NEW_SSH_PORT=$(shuf -i 2000-9999 -n 1)
+NEW_HTTP_PORT=21673
+NEW_HTTPS_PORT=21675
+
 # allow ports and limit ssh
-sudo ufw allow "${NEW_SSH_PORT}"
-sudo ufw limit "${NEW_SSH_PORT}"
+# sudo ufw allow "${NEW_SSH_PORT}"
+# sudo ufw limit "${NEW_SSH_PORT}"
 sudo ufw allow "${NEW_HTTP_PORT}"
 sudo ufw allow "${NEW_HTTPS_PORT}"
 # change ssh port to a new one
-sudo sed -i "s/#Port 22/Port ${NEW_SSH_PORT}/" /etc/ssh/sshd_config
+# sudo sed -i "s/#Port 22/Port ${NEW_SSH_PORT}/" /etc/ssh/sshd_config
 sudo systemctl restart ssh
 # enable new ufw rules
 echo "y" | sudo ufw enable
@@ -50,7 +51,7 @@ sudo sysctl -p
 sudo iptables -I INPUT -p icmp --icmp-type echo-request -j DROP
 
 echo "Save this data:"
-echo "New root password: ${NEW_PASSWORD}"
-echo "SSH port: ${NEW_SSH_PORT}"
+# echo "New root password: ${NEW_PASSWORD}"
+# echo "SSH port: ${NEW_SSH_PORT}"
 echo "proxy port 1: ${NEW_HTTP_PORT}"
 echo "proxy port 2: ${NEW_HTTPS_PORT}"
